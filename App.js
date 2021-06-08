1df79d2203bce93dc79864fd510e92b63921c3ed
surajidk <surajidk12@gmail.com>
2021-06-08 14:24:08 +0700

    event emit dan util nodejs

--- App.js
+++ App.js
@@ -1,4 +1,33 @@
-let counter = require('./touch')
+let events = require('events')
+let util = require('util')
+
+// let myEmited = new events.EventEmitter();
+// myEmited.on('anEvent', (msg) => {
+//     console.log(msg);
+// })
+
+// myEmited.emit('anEvent', 'bismillah oke')
+
+let Person = function (name, color) {
+    this.name = name;
+    this.color = color;
+}
 
 
-console.log(counter([1,2,3,4,5,6]));
+util.inherits(Person, events.EventEmitter);
+
+let james = new Person('james', 'green');
+let jamet = new Person('jamet', 'skyblue');
+let jande = new Person('jande', 'blue');
+
+let People = [james, jamet, jande];
+
+People.forEach((person) => {
+    person.on('speak', (msg) => {
+        console.log(`${person.name} said ${msg} ${person.color}`);
+    })
+})
+
+james.emit('speak', 'apha kabar', 'bisa jadi')
+jamet.emit('speak', 'apha kabar')
+jande.emit('speak', 'apha kabar')
