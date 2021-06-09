a42600fafae4a855aaec90733719cba4527c6c86
surajidk <surajidk12@gmail.com>
2021-06-09 08:37:29 +0700

    create and remove file dir

--- App.js
+++ App.js
@@ -1,6 +1,25 @@
 let fs = require('fs');
 
-fs.readFile('readMe.txt', 'utf8', function (err, data) {
+fs.readFile('readMe.txt', 'utf8', (err, data) => {
     console.log(data);
-    fs.writeFileSync('writeMe.txt',`tambahan data : ${data}`);
+    fs.writeFileSync('writeMe.txt', `tambahan data : ${data}`);
 });
+
+setTimeout(() => {
+    fs.unlinkSync('writeMe.txt'); ``
+}, 1000);
+
+
+// make folder and file
+fs.mkdir('stuff', function () {
+    fs.readFile('readMe.txt', 'utf8', (err, data) => {
+        console.log(data);
+        fs.writeFileSync('./stuff/data.php', data);
+    })
+});
+
+// setTimeout(() => {
+//     fs.unlink('./stuff/data.php', () => {
+//         fs.rmdirSync('stuff');
+//     })
+// }, 3000);
