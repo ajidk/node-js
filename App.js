27abc68b2b012b6144257fc5da9c7bef1566da2f
surajidk <surajidk12@gmail.com>
2021-06-09 20:34:04 +0700

    serving to html, json , etc

--- App.js
+++ App.js
@@ -1,19 +1,24 @@
 let http = require('http');
 let fs = require('fs');
 
-let readStream = fs.createReadStream('readMe.txt');
-let createStream = fs.createWriteStream('writeMe.txt');
+let server = http.createServer((req, res) => {
+    console.log(`url : ${req.statusCode}`);
+    // res.writeHead(200, { 'Content-type': 'text/html' });
+    res.writeHead(200, { 'Content-type': 'application/json' });
 
-readStream.on('data', (chunk) => {
-    console.log('new chunk recieve');
-    createStream.write(chunk);
+
+    // let readStream = fs.createReadStream('index.html');
+    // let createStream = fs.createWriteStream('writeMe.txt');
+
+    // readStream.pipe(res)
+    let obj = {
+        name:'ajidk',
+        major:'backend',
+        from:'sumatra',
+    }
+    // res.end(JSON.stringify(obj))
+    res.end(obj)
 })
 
-// let server = http.createServer((req, res) => {
-//     console.log(`url : ${req.statusCode}`);
-//     res.writeHead(200, { 'Content-type': 'text/plain' });
-//     res.end('the ajidk');
-// })
-
-// server.listen(3000, '127.0.0.1');
-// console.log(`the listen 127.0.0.1:3000`);
+server.listen(3000, '127.0.0.1');
+console.log(`the listen http://127.0.0.1:3000`);
